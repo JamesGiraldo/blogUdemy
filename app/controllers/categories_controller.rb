538b3dc1745fbb142e75c1d8674d9cacfc62a016
jamesgiraldo <jamesgiiraldo@gmail.com>
2020-05-30 22:57:01 -0500

    genial :v section 13 de udemy category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,8 +27,8 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     respond_to do |format|
-      if @category.save        
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+      if @category.save
+        format.html { redirect_to @category, notice: 'La categoría se creó correctamente.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to @category, notice: 'La categoría se actualizó correctamente.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: 'La categoría fue destruida con éxito.' }
       format.json { head :no_content }
     end
   end
